06562891a2fcd5b628e26c92d53b20d5ea10aa5a
Linakhenfri <linakhenfri2004@gmail.com>
2026-05-03 07:50:03 +0100

    Update ProductCard.jsx

--- project-store/src/components/ProductCard.jsx
+++ project-store/src/components/ProductCard.jsx
@@ -9,63 +9,49 @@ export default function ProductCard({ product }) {
 
   const [added, setAdded] = useState(false);
 
+  if (!product) return null;
+
   const inWishlist = isInWishlist(product.id);
 
   const handleAdd = () => {
     addToCart(product);
     setAdded(true);
-
-    // يرجع للوضع الطبيعي بعد 2 ثواني
     setTimeout(() => setAdded(false), 2000);
   };
 
   return (
-    <div className="bg-white dark:bg-gray-900 border border-secondary dark:border-gray-700 p-4 rounded-lg shadow-sm hover:shadow-lg transition flex flex-col relative">
-      
-      {/* ❤️ زر المفضلة */}
+    <div className="p-4 border rounded-lg flex flex-col relative">
+
       <button
         onClick={() => toggleWishlist(product)}
-        className="absolute top-2 right-2 text-xl"
+        className="absolute top-2 right-2"
       >
         {inWishlist ? "❤️" : "🤍"}
       </button>
 
       <Link to={`/product/${product.id}`} className="flex-1">
-        <div className="h-48 flex items-center justify-center overflow-hidden mb-4">
-          <img 
-            src={product.thumbnail} 
-            alt={product.title} 
-            className="max-h-full object-contain" 
-          />
-        </div>
+        <img 
+          src={product?.thumbnail} 
+          alt={product?.title} 
+        />
 
-        <h3 className="text-sm font-semibold truncate text-textDark dark:text-gray-100">
-          {product.title}
+        <h3>
+          {product?.title || "No title"}
         </h3>
 
-        <p className="text-xs mt-1 capitalize text-textDark dark:text-gray-300">
-          {product.category}
+        <p>
+          {product?.category || "No category"}
         </p>
       </Link>
 
-      <div className="flex justify-between items-center mt-3">
-        <span className="text-primary font-bold">
-          ${product.price}
+      <div className="flex justify-between mt-3">
+        <span>
+          ${product?.price || 0}
         </span>
 
-        {/* 🛒 زر Add to Cart */}
-        <button
-          onClick={handleAdd}
-          className={`px-3 py-1 rounded text-xs font-bold transition-all duration-200
-            ${
-              added
-                ? "bg-white text-primary border border-primary cursor-default !hover:bg-white !hover:text-primary"
-                : "bg-secondary dark:bg-gray-700 text-textDark dark:text-gray-100 hover:bg-primary dark:hover:bg-gray-600 hover:text-white"
-            }`}
-        >
-          {added ? "Added ✓" : "Add to Cart"}
+        <button onClick={handleAdd}>
+          {added ? "Added ✓" : "Add"}
         </button>
-
       </div>
     </div>
   );
